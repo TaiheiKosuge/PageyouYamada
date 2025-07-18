40bc1643cad237cf7aabe6eff0a1d23eda0cb66f
小菅太平 <kosugetaihei@kosugataheinoMacBook-Air.local>
2025-07-18 18:18:02 +0900

    最新バージョンのアップロード

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -16,7 +16,7 @@ export async function POST(req: NextRequest) {
   try {
     await transporter.sendMail({
       from: 'taiksg17@gmail.com',
-      to: 'taiksg17@gmail.com', // ここに受信したいアドレス
+      to: 'sinnkennhaiou@gmail.com', // ここに受信したいアドレス
       subject: 'お問い合わせフォームからのメッセージ',
       text: `お名前: ${name}\nメール: ${email}\n内容:\n${message}`,
     });
